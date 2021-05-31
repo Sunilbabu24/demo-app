306fb7f35c89098b431eeaff84a8fffb68e7cdf9
Sunilbabu24git config --global user.name Sunilbabu24git config --global user.name Sunilbabu24 <16pa1a0260@vishnu.edu.in>
2021-05-31 18:08:18 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list] = useState([1, 2, 3, 4, 5, 1, 1, 2, 3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary sticky-top p-3">Project title</h1>
+
+      {list.map((items, index) => (
+        <div className="alert-primary p-3 rounded mb-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolore ab
+          voluptate libero et. Ducimus soluta facilis eum itaque? Placeat veniam
+          iusto maxime laborum, commodi est impedit et nemo nobis.100
+        </div>
+      ))}
     </div>
   );
 }
