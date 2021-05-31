13d8af3f309cfda38ab6f0bb11ad62083a797376
Sunilbabu24git config --global user.name Sunilbabu24git config --global user.name Sunilbabu24 <16pa1a0260@vishnu.edu.in>
2021-05-31 18:30:15 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {}
+function App() {
+  const [list] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  return (
+    <div>
+      <h1 className="sticky-top bg-danger p-2">Instagram</h1>
+
+      {list.map((items, index) => (
+        <div
+          className="alert-primary m-3"
+          style={{ height: "300px" }}
+          key={index}
+        >
+          <img
+            src={"https://picsum.photos/200" + index}
+            alt=""
+            className="rounded"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
