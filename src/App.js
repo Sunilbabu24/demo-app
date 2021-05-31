febadd275b47a97b65ed21cf52a79ed17262fd08
Sunilbabu24git config --global user.name Sunilbabu24git config --global user.name Sunilbabu24 <16pa1a0260@vishnu.edu.in>
2021-05-31 18:11:28 +0530

    as

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  const [list] = useState([1, 2, 3, 4, 5, 1, 1, 2, 3]);
-
-  return (
-    <div>
-      <h1 className="bg-primary sticky-top p-3">Project title</h1>
-
-      {list.map((items, index) => (
-        <div className="alert-primary p-3 rounded mb-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolore ab
-          voluptate libero et. Ducimus soluta facilis eum itaque? Placeat veniam
-          iusto maxime laborum, commodi est impedit et nemo nobis.100
-        </div>
-      ))}
-    </div>
-  );
-}
+function App() {}
 
 export default App;
